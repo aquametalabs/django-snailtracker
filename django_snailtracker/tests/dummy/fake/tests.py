8ef4f469d1ebd09de26685efcffd4633b41b9673
Martin Chase <outofculture@gmail.com>
2012-08-08 15:42:24 -0700

    - added test of parent snailtracking

--- django_snailtracker/tests/dummy/fake/tests.py
+++ django_snailtracker/tests/dummy/fake/tests.py
@@ -5,9 +5,10 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 from django_snailtracker.models import Snailtrack, Action
-from fake.models import MockModel
+from fake.models import MockModel, ChildMockModel
 from nose.tools import assert_equal
 import django_snailtracker
 
@@ -23,3 +24,15 @@ class SnailtrackTest(TestCase):
         mm = MockModel.objects.create(name='test')
         assert_equal(Snailtrack.objects.count(), track_count_before + 1)
         assert_equal(Action.objects.count(), action_count_before + 1)
+
+    def test_children_create_snailtracks_for_their_parents(self):
+        mm = MockModel.objects.create(name='test')
+        mm_content_type = ContentType.objects.get_for_model(MockModel)
+        mm_actions = Action.objects.filter(snailtrack__object_id=mm.pk, snailtrack__content_type=mm_content_type)
+        action_count_before = mm_actions.count()
+        cmm = ChildMockModel.objects.create(name='kid', mock_model=mm)
+        child_content_type = ContentType.objects.get_for_model(ChildMockModel)
+
+        c_track = Snailtrack.objects.get(object_id=cmm.pk, content_type=child_content_type)
+        assert_equal(c_track.parent.content_object, mm)
+        assert_equal(mm_actions.count(), action_count_before)
